290e01d80a6b6d33eab8e24a2af32f40075bc0c3
Shreyas Singh <shraeyassingh@gmail.com>
2020-08-14 13:59:30 +0530

    Update progressBar.py

--- progressBar.py
+++ progressBar.py
@@ -3,7 +3,8 @@ def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, 
     percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
     filledLength = int(length * iteration // total)
     bar = fill * filledLength + '-' * (length - filledLength)
-    print(f'\r{prefix} |{bar}| {percent}% {suffix}', end = printEnd)
+    #print(f'\r{prefix} |{bar}| {percent}% {suffix}', end = printEnd)
+    print("\r" + str(percent), end="")
     # Print New Line on Complete
     if iteration == total: 
         print("\n")

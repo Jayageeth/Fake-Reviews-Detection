4c0da7d351fa1d33a9dfc3d8ee9e9cf20c06ec5c
Shreyas Singh <shraeyassingh@gmail.com>
2020-08-15 12:28:08 +0530

    Graph save in disk

--- graph.py
+++ graph.py
@@ -103,11 +103,11 @@ def plot2d(X, y, y_pred_bernoulli, y_pred_svc):
     plt.plot(X_pred_svc_red, X_pred_svc_red_points, c = 'green', linestyle = 'dashed', label='SVC Fake Reviews Predicted')
     plt.plot(X_pred_svc_blue, X_pred_svc_blue_points, c = 'green', linestyle = 'solid', label='SVC True Reviews Predicted')
 
-    plt.legend()
-    plt.show();
-
     plt.savefig(os.path.join('result', 'plot2d.png'))
 
+    plt.legend()
+    plt.show()
+    
 def plot_comp(y_test, y_pred_bernoulli, y_pred_svc) :
         plt.figure()
 

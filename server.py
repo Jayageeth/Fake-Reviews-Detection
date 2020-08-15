4251067c48b494dc99eb22202c2618ae758af376
Shreyas Singh <shraeyassingh@gmail.com>
2020-08-15 13:18:27 +0530

    Added ngrok for colab

--- server.py
+++ server.py
@@ -11,21 +11,28 @@ def homepage ():
 @app.route ('/api/bot', methods = ['POST'])
 def say_name ():
 	json = request.get_json()
-	
+
 	review_text = request.form['review_text']
 	rating = request.form['rating']
 	verified_purchase =  request.form['verified_purchase']
 	product_category = request.form['product_category']
-	
+
 	result =  deploy.get_result(review_text, rating, verified_purchase, product_category)
 	print (result)
-	
+
 	if result[0] == 1:
 		return jsonify (result = 'T')
-		
+
 	else:
 		return jsonify (result = 'F')
-	
-	
+
+from pyngrok import ngrok
+
+def config_ngrok() :
+    app.config['START_NGROK'] = True
+    url = ngrok.connect(5000)
+    print(' * Public URL:', url)
+
 if __name__ == '__main__':
-	app.run ()
+    config_ngrok()
+    app.run ()
